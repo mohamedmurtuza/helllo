954367ad5d8302e2720c0b16f7bd3be1587d3661
mohamedmurtuza <mohamedmurtuza3@gmail.com>
2020-05-03 22:26:31 +0530

    near to success what you say

--- src/What.java
+++ src/What.java
@@ -7,7 +7,17 @@ public class What {
 		System.out.println("I am not fine");
 		System.out.println("What do you want");
 		System.out.println("Nothing");
+		
+		
+		 hello ();
 
+	}
+	
+	public static void hello () {
+		
+		System.out.println(" i am very much happy u know");
+		System.out.println("Why");
+		System.out.println("No Idea");
 	}
 
 }
